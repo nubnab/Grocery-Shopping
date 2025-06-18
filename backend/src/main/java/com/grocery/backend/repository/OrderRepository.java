1571449f6e919c59d12cf4477a13f0d9938648c5
nubnab <emkata654@gmail.com>
2025-06-18 19:46:42 +0300

    Added initial repositories

---
+++ backend/src/main/java/com/grocery/backend/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.grocery.backend.repository;
+
+import com.grocery.backend.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
---
+++ backend/src/main/java/com/grocery/backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.grocery.backend.repository;
+
+import com.grocery.backend.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
